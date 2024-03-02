65ddf06c098441699452b2cc54a6f7232fb4a1d6
EdSabhin <edsabhin@yahoo.com>
2024-03-02 09:05:06 -0500

    Improved Navbar appearance.

--- src/component-props/navProps.ts
+++ src/component-props/navProps.ts
@@ -2,18 +2,16 @@ const navProps = {
   shakespeare: {
     className:
       "w-full p-8 bg-gradient-to-r from-blue-950 via-slate-700 to-gray-900",
-    ulClassName:
-      "w-full flex justify-center pb-1 gap-14 text-3xl text-transparent bg-clip-text bg-gradient-to-r from-amber-50 to-orange-200",
+    ulClassName: "w-full flex justify-around text-3xl",
     linkClassName:
-      "hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-amber-100 hover:to-orange-300",
+      "pb-1 text-transparent bg-clip-text bg-gradient-to-r from-amber-50 to-orange-300 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-amber-100 hover:to-orange-400",
   },
   dickinson: {
     className:
       "w-full p-8 bg-gradient-to-r from-indigo-100 to-rose-100 text-amber-700",
-    ulClassName:
-      "w-full flex justify-center pb-1 gap-14 text-3xl text-transparent bg-clip-text bg-gradient-to-r from-rose-800 to-teal-500",
+    ulClassName: "w-full flex justify-around text-3xl",
     linkClassName:
-      "hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-rose-800 hover:to-teal-900",
+      "pb-1 text-transparent bg-clip-text bg-gradient-to-r from-rose-600 to-teal-600 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-rose-800 hover:to-teal-800",
   },
 }
 
